51fdf5525f155b682e44a6a94c94673c5ead7700
Santiago Manso <santiagomanso@web.de>
2022-05-13 03:19:37 +0200

    initial commit

---
+++ src/HeroesApp.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import AppRouter from './components/routers/AppRouter'
+
+const HeroesApp = () => {
+  return <AppRouter />
+}
+
+export default HeroesApp
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,38 @@
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+  const handlerLogOut = () => {
+    //TODO:
+    alert('log out')
+  }
+
+  return (
+    <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+      <Link className='navbar-brand' to='/'>
+        Heroes App
+      </Link>
+
+      <div className='navbar-collapse'>
+        <div className='navbar-nav'>
+          <NavLink className='nav-item nav-link' to='/marvel'>
+            Marvel
+          </NavLink>
+
+          <NavLink className='nav-item nav-link' to='/dc'>
+            DC
+          </NavLink>
+        </div>
+      </div>
+
+      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
+        <span className='nav-item nav-link text-info'> user</span>
+
+        <ul className='navbar-nav ml-auto'>
+          <button className='nav-item nav-link btn' onClick={handlerLogOut}>
+            Logout
+          </button>
+        </ul>
+      </div>
+    </nav>
+  )
+}
